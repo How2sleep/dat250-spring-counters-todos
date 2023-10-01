39ebdcd0611cbf805144026cad520efecc1a7478
Magnus <magnus.aune@student.uib.no>
2023-10-01 14:27:25 +0200

    finito

--- src/main/java/no/hvl/dat250/rest/todos/TodoController.java
+++ src/main/java/no/hvl/dat250/rest/todos/TodoController.java
@@ -1,10 +1,10 @@
 package no.hvl.dat250.rest.todos;
 
-import no.hvl.dat250.rest.counters.Counters;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.Serial;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Rest-Endpoint for todos.
@@ -12,34 +12,72 @@ import java.io.Serial;
 @RestController
 public class TodoController {
 
-  private Todo todo = new Todo();
+  private List<Todo> todoList = new ArrayList<>();
 
   public static final String TODO_WITH_THE_ID_X_NOT_FOUND = "Todo with the id %s not found!";
 
   @GetMapping("/todos")
-  public Todo getTodos() {
-    return todo ;
+  public List<Todo> getTodos() {
+    return todoList;
   }
 
   @GetMapping("/todos/{id}")
-    public Todo getTodoById(@PathVariable Long id) {
-        if (todo.getId() == id) {
-        return todo;
-        } else {
-        return null;
+    public Object getTodoById(@PathVariable Long id) {
+        for (Todo todo : todoList) {
+            if (todo.getId().equals(id)) {
+                return todo;
+            }
+        }
+        return createJsonErrorMessage(String.format( TODO_WITH_THE_ID_X_NOT_FOUND, id));
+    }
+  @PostMapping("/todos")
+  public Todo createTodo(@RequestBody Todo newTodo) {
+      if (newTodo.getId() == null) {
+          newTodo.setId((long) (todoList.size() + 1));
+      }
+    todoList.add(newTodo);
+    return newTodo;
+  }
+  @PutMapping("/todos/{id}")
+    public Object updateTodoById(@PathVariable Long id, @RequestBody Todo newTodo) {
+        todoList = getTodos();
+        for (Todo todo : todoList) {
+            if (todo.getId().equals(id)) {
+                todo.setSummary(newTodo.getSummary());
+                todo.setDescription(newTodo.getDescription());
+                return todo;
+            }
+        }
+        return createJsonErrorMessage(String.format( TODO_WITH_THE_ID_X_NOT_FOUND, id));
+    }
+
+    @DeleteMapping("/todos/{id}")
+    public Object deleteTodoById(@PathVariable Long id) {
+        todoList = getTodos();
+        for (Todo todo : todoList) {
+            if (todo.getId().equals(id)) {
+                todoList.remove(todo);
+                return todoList;
+            }
+        }
+        return createJsonErrorMessage(String.format( TODO_WITH_THE_ID_X_NOT_FOUND, id));
+    }
+    static String createJsonErrorMessage(String message) {
+        Gson gson = new Gson();
+        ErrorMessage errorMessage = new ErrorMessage(message);
+        return gson.toJson(errorMessage);
+    }
+
+    // Create a class to represent the error message structure
+    static class ErrorMessage {
+        private String message;
+
+        public ErrorMessage(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
         }
     }
-  @PutMapping("/todos")
-  @Ge
-  public Todo createTodo() {
-    todo = new Todo();
-    return todo;
-  }
-
-    @DeleteMapping("/todos")
-    public Todo deleteTodo() {
-        todo = new Todo();
-        return todo;
-    }
-
 }
